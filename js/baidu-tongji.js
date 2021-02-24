3a658391829825a64c2b8ea4745b9e76356b5305
CnzOOOo <CnzOOOo@163.com>
2021-02-24 20:34:29 +0800

    Site updated: 2021-02-24 20:34:28

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?a9360dfc05a669bb4d582a56430ed9d0";
+  hm.src = "https://hm.baidu.com/hm.js?8158ec11e2a46accd7ad6de7dd775f8f";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
